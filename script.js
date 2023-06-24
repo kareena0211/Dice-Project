5294a472fccc71e6b97c37abae3812434080e0f8
kareena22 <kareena22@navgurukul.org>
2023-06-25 01:07:00 +0530

    Added script part

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("start").addEventListener("click", function () {
+
+    let p1 = Math.floor(Math.random() * 6) + 1;
+    let p2 = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("p1").src = `image/${p1}.png`;
+    document.getElementById("p2").src = `image/${p2}.png`;
+    if (p1 > p2)
+        document.querySelector("h1").textContent = "🥳 Player 1 wins 🥳";
+    else if (p2 > p1)
+        document.querySelector("h1").textContent = " 🤩 Player 2 wins 🤩";
+    else
+        document.querySelector("h1").textContent = " 🤗 Draw 🤗";
+
+})
